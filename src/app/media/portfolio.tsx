bb77dc44c8361810be580212b0cbe1e9b1c930fa
bfung <bfung0011@gmail.com>
2023-08-17 01:19:47 +0700

    finish contacts

---
+++ src/app/media/portfolio.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf"
+import workerSrc from "../pdf-worker"
+
+
+function BCRport() {
+    return (
+        
+    )
+}
